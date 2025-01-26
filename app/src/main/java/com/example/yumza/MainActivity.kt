7cf97b276a64ae626751c7065a066afd39267328
Harpreet Singh <harpreetsingh@Harpreets-MacBook-Air.local>
2025-01-26 16:13:48 +0530

    first page of Yumza

--- app/src/main/java/com/example/yumza/MainActivity.kt
+++ app/src/main/java/com/example/yumza/MainActivity.kt
@@ -11,6 +11,7 @@ import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
