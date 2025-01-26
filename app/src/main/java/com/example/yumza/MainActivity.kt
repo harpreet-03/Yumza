8b0d66c7396b62e195034c9e655f7333282003ea
Harpreet Singh <harpreetsingh@Harpreets-MacBook-Air.local>
2025-01-26 16:08:05 +0530

    Yumza App - first Page of App

---
+++ app/src/main/java/com/example/yumza/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.yumza
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.getStarted)
+
+        button.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
